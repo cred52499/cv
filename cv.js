6805b61304d8aed38ea447e575af458d50224cd3
darrenlau <darren.lau.xh@gmail.com>
2024-04-18 03:37:38 +0800

    Skills section update

--- cv.js
+++ cv.js
@@ -23,3 +23,36 @@ appearOptions);
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 });
+
+const observer1 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('circle1');
+        }
+        });
+        
+});
+
+observer1.observe(document.querySelector('.java'));
+
+const observer2 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('circle2');
+        }
+        });
+        
+});
+
+observer2.observe(document.querySelector('.python'));
+
+const observer3 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('circle3');
+        }
+        });
+        
+});
+
+observer3.observe(document.querySelector('.javascript'));
