f77049f7e3a563611e040e6274e0534cd238c375
darrenlau <darren.lau.xh@gmail.com>
2024-04-18 07:58:59 +0800

    Updated certification section

--- cv.js
+++ cv.js
@@ -56,3 +56,29 @@ const observer3 = new IntersectionObserver(entries => {
 });
 
 observer3.observe(document.querySelector('.javascript'));
+
+const track = document.getElementById("image-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+
+}
+
+window.onmousemove = e => {
+    if(track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
+    const percentage = (mouseDelta / maxDelta) * -100,
+    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -128.5);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.style.transform = `translate(${nextPercentage}%, 0)`;
+}
