07035c2a7fdfc63f6f437a777f8c77554b6f31d2
darrenlau <darren.lau.xh@gmail.com>
2024-04-18 09:19:15 +0800

    updated certification and navbar

--- cv.js
+++ cv.js
@@ -73,7 +73,9 @@ window.onmouseup = () => {
 window.onmousemove = e => {
     if(track.dataset.mouseDownAt === "0") return;
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, maxDelta = window.innerWidth / 2;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
+    maxDelta = window.innerWidth / 2;
+    
     const percentage = (mouseDelta / maxDelta) * -100,
     nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
     nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -128.5);
@@ -82,3 +84,4 @@ window.onmousemove = e => {
 
     track.style.transform = `translate(${nextPercentage}%, 0)`;
 }
+
